542462b665b628efe63e0ead446bb4767de8452e
laurentiuVasilescu2001 <laurentiumarian2342@gmail.com>
2023-01-21 13:18:04 +0200

    first commit

---
+++ inginerie-software/src/main/java/com/example/ingineriesoftware/InginerieSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingineriesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InginerieSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InginerieSoftwareApplication.class, args);
+    }
+
+}
