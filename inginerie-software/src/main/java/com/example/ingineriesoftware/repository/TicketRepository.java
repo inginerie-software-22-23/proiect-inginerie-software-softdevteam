383779030d613ae52dfb857613295a571b9fcf92
laurentiuVasilescu2001 <laurentiumarian2342@gmail.com>
2023-01-23 17:07:17 +0200

    added repository

--- inginerie-software/src/main/java/com/example/ingineriesoftware/repository/TicketRepository.java
+++ inginerie-software/src/main/java/com/example/ingineriesoftware/repository/TicketRepository.java
@@ -4,4 +4,5 @@ import com.example.ingineriesoftware.model.Ticket;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TicketRepository extends CrudRepository<Ticket, Integer> {
+
 }
